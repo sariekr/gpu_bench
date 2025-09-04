83a846f388960f84a9a2a10515530f4ab0c922e5
Ekrem Sarı <ekrem.sari@aimultiple.com>
2025-09-04 13:12:06 +0300

    added amd

--- monitor_gpu.sh
+++ monitor_gpu.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 OUTPUT_FILE=$1
 GPU_TYPE=$2
-echo "timestamp,gpu_id,memory.used_GB" > $OUTPUT_FILE
+echo "timestamp,gpu_id,memory.used_GB" > "$OUTPUT_FILE"
 
 if [ "$GPU_TYPE" = "nvidia" ]; then
   while true; do
-    nvidia-smi --query-gpu=timestamp,gpu_uuid,memory.used --format=csv,noheader,nounits | awk -F, '{print $1 "," $2 "," $3/1024}' >> $OUTPUT_FILE
+    nvidia-smi --query-gpu=timestamp,gpu_uuid,memory.used --format=csv,noheader,nounits | awk -F, '{print $1 "," $2 "," $3/1024}' >> "$OUTPUT_FILE"
     sleep 0.1
   done
 else # amd
   while true; do
-    rocm-smi --showmemused --csv | tail -n +2 | awk -F, '{print strftime("%Y/%m/%d %H:%M:%S.000") "," $1 "," $2/1024}' >> $OUTPUT_FILE
+    # --showmemused yerine daha uyumlu olan --showmemuse kullanıldı.
+    rocm-smi --showmemuse -d 0 | tail -n 1 | awk '{print strftime("%Y/%m/%d %H:%M:%S.000") ",0," $5/1024}' >> "$OUTPUT_FILE"
     sleep 0.1
   done
 fi
