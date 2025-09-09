aad6206eada780ec39b8b1c628fdc1a24fc06ead
Ekrem Sarı <ekrem.sari@aimultiple.com>
2025-09-09 23:59:31 +0300

    mi300x fix

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -61,11 +61,8 @@ do
   elif [ "$GPU_BRAND" = "amd" ]; then
     echo "INFO: Running AMD official optimized command..."
     # --- AMD İÇİN KRİTİK PERFORMANS AYARLARI ---
-    export VLLM_USE_TRITON_FLASH_ATTN=0
     export VLLM_V1_USE_PREFILL_DECODE_ATTENTION=1
-	export VLLM_ROCM_USE_AITER=1
-	export VLLM_USE_AITER_UNIFIED_ATTENTION=1
-	export VLLM_ROCM_USE_AITER_MHA=0
+
 **
     
     # Dökümandaki optimize edilmiş komut
